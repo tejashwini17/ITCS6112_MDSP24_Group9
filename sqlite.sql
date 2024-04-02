9824064256deb18655f69036dde4ba8f5f10c064
luckylakshith <luckylakshith6@gmail.com>
2024-04-01 23:02:56 -0400

    Changes

--- sqlite.sql
+++
@@ -1,3 +0,0 @@
-UPDATE auth_user_groups
-SET group_id = 1
-WHERE user_id = 36;
--- store/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class StoreConfig(AppConfig):
-    name = 'store'
-
-    def ready(self):
-        import store.signals
-
